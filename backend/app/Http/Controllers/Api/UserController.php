4e97fdd1d51db7f9f29a4a3c6766af85d8a52eb2
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-05-07 20:59:47 +0100

    Update UserController.php (backend)

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -72,7 +72,7 @@ class UserController extends Controller
     public function showProjects(User $user)
     {
         $projects = $user->projects()
-            ->withCount('comments')
+            ->withCount(['views', 'comments'])
             ->latest()
             ->paginate(10);
 
