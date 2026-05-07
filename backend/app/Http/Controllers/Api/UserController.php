bdb39769d4cd803a90be29667d6959836fc2a438
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-05-07 20:21:32 +0100

    Update UserController.php (backend)

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -65,13 +65,17 @@ class UserController extends Controller
         $user->is_following = $authUser->following()
             ->where('following_id', $user->id)
             ->exists();
-    
+
         return response()->json($user);
     }
 
     public function showProjects(User $user)
     {
-        $projects = $user->projects()->latest()->paginate(10);
+        $projects = $user->projects()
+            ->withCount('comments')
+            ->latest()
+            ->paginate(10);
+
         return response()->json($projects);
     }
 }
